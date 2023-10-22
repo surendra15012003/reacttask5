837421a7a0d1ac7a806453cc3866197a59d169a7
surendra15012003 <surendrabond10@gmail.com>
2023-10-22 13:43:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+import AgeCalculator from './components/AgeCalculator';
+
+class App extends Component {
+  render() {
+    return (
+      <AgeCalculator/>
+    );
+  }
+}
+
+export default App;
